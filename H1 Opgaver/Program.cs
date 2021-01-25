c5b39cbec1893f0279f1d397bcef93f8b82d483c
Simranjit Singh Gulati <simr0026@zbc.dk>
2021-01-25 09:56:40 +0100

    Initial commit

--- H1 Opgaver/Program.cs
+++ H1 Opgaver/Program.cs
@@ -6,7 +6,16 @@ namespace H1_Opgaver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 16;
+            double b = 5.1;
+            
+            //int c = a + b;
+            //Dette virker ikke da jeg prøver at få en int til at bruge en double værdi hvilket ikke er muligt siden int kun er til heltal.
+            
+            double d = a + b;
+            //Dette virker fordi en double kan både vise heltal samt decimaltal.
+            
+            Console.WriteLine(d);
         }
     }
 }
