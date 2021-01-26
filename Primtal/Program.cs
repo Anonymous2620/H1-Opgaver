f179bff0b9b1ef0f25f694d72a6099ddc936fa3f
Simranjit Singh <Simranjit.Singh@outlook.dk>
2021-01-26 22:24:50 +0100

    Primtal

---
+++ Primtal/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Primtal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                try
+                {
+                
+                Console.WriteLine("\nWrite a number which will be the range all " +
+                    "prime numbers will be shown within that range in the colour red");
+                
+                    int input = int.Parse(Console.ReadLine());
+                
+                    for (int a = 2; a < input; a++)
+                    {
+                        int prime = 0;
+                        
+                        for (int b = 1; b < a; b++)
+                        {
+                            if (a % b == 0)
+                                
+                                prime++;
+                
+                            if (prime == 2)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Gray;
+                                Console.WriteLine(a);
+                                break;
+                            }
+                
+                        }
+                        
+                        if (prime != 2)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(a);
+                        }
+                
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid input");
+                    Console.ReadKey();
+                    Console.Clear();
+                    continue;
+                   
+                }
+                
+            }
+            
+        }
+    }
+}
