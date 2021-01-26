fc24351880e3ba78a9f71f5700c2717c226b71bb
Simranjit Singh Gulati <simr0026@zbc.dk>
2021-01-26 03:10:36 +0100

    In the proccces of lidt matematik

---
+++ Lidt matematik/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lidt_matematik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Input 2 integers and you will get answer in Adition, Division and Potency");
+
+
+                    Console.WriteLine("Write integer one:");
+                    int one = int.Parse(Console.ReadLine());
+
+
+                    Console.WriteLine("Write integer two:");
+                    int two = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("The numbers plussed together: " + (one + two));
+
+
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid input");
+                    continue;
+                }
+            }
+        }
+    }
+}
