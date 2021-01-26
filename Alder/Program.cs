f43fe8954bf5082b94c62ab2172bc7ca58096b5f
Simranjit Singh <Simranjit.Singh@outlook.dk>
2021-01-26 06:57:03 +0100

    Alder

---
+++ Alder/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Random random = new Random();
+                int nothing = random.Next(3, 15);
+
+                try
+                {
+                    Console.WriteLine("Enter your name to get startet");
+                    string name = (Console.ReadLine());
+                    Console.WriteLine("Now enter your age");
+                    int age = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Name: " + name);
+                    Console.WriteLine("Age: " + age);
+
+                    if
+                    (age < 3)
+                        Console.WriteLine(name + ", du må gå med ble");
+
+                    else if
+                        (age >= 18)
+                        Console.WriteLine(name + ", du må stemme og køre bil");
+
+                    else if (age == 17)
+                        Console.WriteLine(name + ", du må drikke");
+
+                    else if (age == 16)
+                        Console.WriteLine(name + ", du må drikke");
+
+                    else if (age <= 15)
+                        Console.WriteLine(name + ", du må ingenting");
+
+                    else if (age >= 3)
+                        Console.WriteLine(name + ", du må ingenting");
+
+
+                }
+                catch
+                {
+                    Console.WriteLine("INVALID INPUT!!!");
+                    continue;
+
+                }
+
+
+            }
+        }
+    }
+}
---
+++ Pythagoras/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Pyhtagoras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+
+                    Console.WriteLine("\nPythagoras calculator.");
+                    Console.WriteLine("\nWrite a^2:");
+                    double a2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("\nWrite b^2:");
+                    double b2 = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nc^2 is: : " + (a2 * a2 + b2 * b2));
+
+                    if (a2 > b2)
+                    {
+                        Console.WriteLine("\na^2 is the bigger number");
+
+                    }
+                    else if (a2 < b2)
+                    {
+                        Console.WriteLine("\nb^2 is the bigger number");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nBoth a^2 and b^2 is the same number");
+                    }
+
+
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid input");
+                    continue;
+                }
+
+            }
+
+        }
+    }
+}
+
+
